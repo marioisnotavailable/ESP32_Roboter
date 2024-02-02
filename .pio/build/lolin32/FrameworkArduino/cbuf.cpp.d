ffb16c8c549523cd425662cabb31a330f81bf10e
marioisnotavailable <150730416+marioisnotavailable@users.noreply.github.com>
2024-02-02 13:39:26 +0000

    fdh

--- .pio/build/lolin32/FrameworkArduino/cbuf.cpp.d
+++ .pio/build/lolin32/FrameworkArduino/cbuf.cpp.d
@@ -1,3 +1,3 @@
 .pio/build/lolin32/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/Super/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/Super/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
+ C:/Users/20220067/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/20220067/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
