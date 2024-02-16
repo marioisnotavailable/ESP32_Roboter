09e115de9fc92b585e42cd3a9f364d896a7eaa12
Mario <mario.haunold07@gmail.com>
2024-02-16 13:19:52 +0100

    cx

--- .pio/build/lolin32/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/lolin32/FrameworkArduino/USB.cpp.o: \
- C:/Users/Super/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- C:/Users/Super/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/Super/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
