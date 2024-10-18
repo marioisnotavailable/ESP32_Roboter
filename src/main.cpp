c633bdc8bbeb56dca9e0bd3a3a8f8f446b3ce54e
marioisnotavailable <150730416+marioisnotavailable@users.noreply.github.com>
2024-10-18 15:36:36 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,48 @@ extern BluetoothSerial SerialBT;
 extern CRGB leds[NUM_LEDS];
 extern float distance;
 
+bool a = true;
+bool b = true;
 unsigned long starttimee = 0;
+int i = 250;
+
 
 void setup(){
   QuickSetup();
+  digitalWrite(MOTL_DIR,LOW);
+  digitalWrite(MOTR_DIR,HIGH);
 }
 
 void loop(){
-  //LoadingProgramm();
   VoltageMonitoring();
   Ultrasonic();
-  Linefollowerfn();
-
-  //if(millis() - starttimee > 1000){
-  //  SerialBT.println(distanceSonic);
-  //  starttimee = millis();
-  //}
+  if (distance < 10)
+  {
+    ledcWrite(0,0);
+    ledcWrite(1,0);
+  }else{
+    digitalWrite(MOTL_DIR,b);
+    digitalWrite(MOTR_DIR,!b);
+  if(millis() - starttimee >= 1){
+    starttimee = millis();   
+     if(a){
+      if (i >= 1024)
+      {
+        a = false;
+      }
+      i++;
+  }else{
+      if (i <= 250)
+      {
+        a = true;
+        b = !
+        
+        b;
+      }
+      i--;
+  }
+  ledcWrite(0,i);
+  ledcWrite(1,i);
+  }
+}
 }
